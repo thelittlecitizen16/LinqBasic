748e04c97352e37c56b7792813b3b9767c80c5a1
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-08-13 08:47:52 +0300

    manage the IPersonRetriver

---
+++ LinqBasic/ManageIPersonRetriver.cs
@@ -0,0 +1,28 @@
+﻿using LinqBasic.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqBasic
+{
+    class ManageIPersonRetriver : IPersonRetriver
+    {
+        public IEnumerable<int> GetMalesId(IEnumerable<Person> persons)
+        {
+            return persons.Where(p => p.PersonGender == Gender.Male)
+                .Select(i => i.Id).ToList();
+        }
+
+
+        public IEnumerable<Person> GetOddIds(IEnumerable<Person> persons)
+        {
+            return persons.OrderByDescending(p => p.Id).ToList();
+        }
+
+        public IEnumerable<Person> OrderById(IEnumerable<Person> persons)
+        {
+            return persons.Where(p => p.Id % 2 == 0).ToList();
+        }
+    }
+}
---
+++ LinqBasic/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LinqBasic.Abstract;
+
+namespace LinqBasic
+{
+    class Program
+    {
+     
+
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.PersonGender = Gender.Male;
+            p1.Id = 123;
+
+            Person p2 = new Person();
+            p2.PersonGender = Gender.Male;
+            p2.Id = 254;
+
+            Person p3 = new Person();
+            p3.PersonGender = Gender.Female;
+            p3.Id = 500;
+
+            List<Person> allPersons = new List<Person>() { p1, p2, p3 };
+            ManageIPersonRetriver manageIPersonRetriver = new ManageIPersonRetriver();
+
+            foreach (var malePersonnId in manageIPersonRetriver.GetMalesId(allPersons))
+            {
+                Console.WriteLine(malePersonnId);
+            }
+
+            foreach (var personnIds in manageIPersonRetriver.GetOddIds(allPersons))
+            {
+                Console.WriteLine(personnIds.Id);
+            }
+
+            foreach (var personnIds in manageIPersonRetriver.OrderById(allPersons))
+            {
+                Console.WriteLine(personnIds.Id);
+            }
+                   }
+    }
+}
